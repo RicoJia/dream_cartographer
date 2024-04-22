248e8769f10d1d36eb1e5decb192c9fe87a93929
ricocoyeah <rjia@diligentrobots.com>
2024-04-21 19:23:32 -0500

    cuda found

---
+++ 2dlocalizer_scripts/build/CMakeFiles/2DLocalizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2DLocalizer"
+  "2DLocalizer.pdb"
+  "CMakeFiles/2DLocalizer.dir/2dlocalizer.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2DLocalizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
